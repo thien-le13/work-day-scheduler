0031fa087307dc607d369dd6a5df208b14fef248
Buhdurpadur <thien.le13@yahoo.com>
2023-01-23 18:15:40 -0800

    Event listener added to each button.

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,21 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
+  // Adds an event listener to each button that will save all the entries at once.
+  var saveButtons = $("[aria-label='save']");
+  var saveText = JSON.parse(localStorage.getItem("saveText")) || [];
+
+  saveButtons.each(function (index) {
+    $(this).on("click", function (event) {
+      saveButtons.each(function (ind) {
+        var id = $(saveButtons[ind]).parent().attr("id");
+        var time = id.replace("hour-", "");
+        saveText[parseInt(time, 10) - 9] = $(saveButtons[ind])
+          .siblings("textarea")
+          .val();
+      });
+
+      localStorage.setItem("saveText", JSON.stringify(saveText));
+    });
+  });
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -20,6 +27,6 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // This displays the current day on the page.
-  var currentDay = $('#currentDay');
-  currentDay.text(dayjs().format('dddd, MMMM D, YYYY'));
+  var currentDay = $("#currentDay");
+  currentDay.text(dayjs().format("dddd, MMMM D, YYYY"));
 });
